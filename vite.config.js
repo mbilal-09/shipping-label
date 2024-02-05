7ae9bc7e5608d4ad900e890fcbcd390af4fdbabb
mbilal-09 <bilalkhanshakir@gmail.com>
2024-02-05 14:23:58 +0500

    vite config fixed

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 5000,
     rollupOptions: {
-      // Example: externalize specific modules
-      external: ['./src/pages/Dashboard'],
+      external: [],
     },
   },
 })
